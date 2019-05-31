cdd643372d56514237c99aa053de8f3b99d3cd08
Natalman <sperobb@outlook.com>
2019-05-31 08:07:27 -0500

    add parcelable to the Movie.java

--- app/src/main/java/com/example/android/popmovie/Movie.java
+++ app/src/main/java/com/example/android/popmovie/Movie.java
@@ -1,6 +1,9 @@
 package com.example.android.popmovie;
 
-public class Movie {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Movie implements Parcelable {
 
     /** Initializing data to be used in the constructor */
 
@@ -31,6 +34,41 @@ public class Movie {
         this.mMovieImage = image;
 
     }
+
+    private Movie (Parcel parcel){
+        mtitle = parcel.readString();
+        mOverview = parcel.readString();
+        mRating = parcel.readInt();
+        mDateRelease = parcel.readString();
+        mMovieImage = parcel.readInt();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(mtitle);
+        dest.writeString(mOverview);
+        dest.writeInt(mRating);
+        dest.writeString(mDateRelease);
+        dest.writeInt(mMovieImage);
+    }
+
+    public final Parcelable.Creator<Movie> CREATOR= new Parcelable.Creator<Movie>(){
+
+        @Override
+        public Movie createFromParcel(Parcel source) {
+            return new Movie(source);
+        }
+
+        @Override
+        public Movie[] newArray(int size) {
+            return new Movie[0];
+        }
+    };
 
     public String getMtitle() {
         return mtitle;
